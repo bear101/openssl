d217f92349c7c96d6f0a415ccc7f52cd0bc22acd
Bjørn Damstedt Rasmussen <contact@bearware.dk>
2019-07-13 14:13:38 +0200

    Expect to be run from VS build console

--- build_win32.bat
+++ build_win32.bat
@@ -1,7 +1,3 @@
 REM Build with ASM: http://www.nasm.us/
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
 perl Configure no-shared no-asm --prefix=%CD%\win32 --openssldir=%CD%\win32\ssl VC-WIN32
 nmake
-pause
-@echo "Run 'nmake clean' before building x86_64"
-pause
--- build_x64.bat
+++ build_x64.bat
@@ -1,7 +1,3 @@
 REM Build with ASM: http://www.nasm.us/
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 perl Configure no-shared no-asm --prefix=%CD%\x64 --openssldir=%CD%\win32\ssl VC-WIN64A
 nmake
-pause
-@echo "Run 'nmake clean' before building win32"
-pause
